9814596fb97758781f4c697a622b8da275e90b7b
Rasmus Thomsen <rasmus0408@gmail.com>
2021-10-19 12:37:09 +0200

    Felter klasse lavet, indeholder alle 11 felter i et String 2d array

---
+++ Matador/src/FelterTester.java
@@ -0,0 +1,6 @@
+public class FelterTester {
+    public static void main(String[] args) {
+        Felter felt = new Felter();
+        System.out.println(felt.getFelt(1,0)+" og "+ felt.getFelt(1,1));
+    }
+}
