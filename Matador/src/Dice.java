ac51d58b4fd5ad902017b88967af32b67fb7a334
Christoffer Fink <christo.fink.1@gmail.com>
2021-10-19 12:26:14 +0200

    gjort terningen agil

--- Matador/src/Dice.java
+++ Matador/src/Dice.java
@@ -1,13 +1,17 @@
 import java.util.Random;
 
 public class Dice {
+
     public int[] get;
     Random dice = new Random();
-
-
+    public int faceValue;
+    public Dice(int faceValue){
+        this.faceValue=faceValue;
+    }
     public int[] getDice() {
-        int dice1 = dice.nextInt(6)+1;
-        int dice2 = dice.nextInt(6)+1;
+        int dice1 = dice.nextInt(faceValue)+1;
+        int dice2 = dice.nextInt(faceValue)+1;
         return new int[]{dice1,dice2};
     }
+
 }
