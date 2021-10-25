3b5243259f4b894e7e17a73264c020a47f759399
Oskar <oskarlolklarsen@gmail.com>
2021-10-25 16:27:04 +0200

    påbegyndt introtext metode

--- Matador/src/BoardGame.java
+++ Matador/src/BoardGame.java
@@ -1,7 +1,21 @@
 public class BoardGame {
     public static void main(String[] args) {
-        Board board = new Board();
-        board.getBoard(1,2);
+
+    }
+
+    public static void introText(){
+        System.out.println("Velkommen til FeltSpillet");
+        System.out.println("--------------------------------------------------------------------------------------------");
+        System.out.println("Man vinder ved at opnå 3000 point");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("--------------------------------------------------------------------------------------------");
+    }
+
+
+
 
     }
 }
