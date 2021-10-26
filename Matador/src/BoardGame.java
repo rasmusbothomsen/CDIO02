b9faf52fd26443a263a88800dc8ed42bbc364df3
Oskar <oskarlolklarsen@gmail.com>
2021-10-26 12:48:45 +0200

    videreskrivning på intro

--- Matador/src/BoardGame.java
+++ Matador/src/BoardGame.java
@@ -6,8 +6,8 @@ public class BoardGame {
     public static void introText(){
         System.out.println("Velkommen til FeltSpillet");
         System.out.println("--------------------------------------------------------------------------------------------");
+        System.out.println("Hver af spillerne skiftes til at slå med 2 terninger og lander på et vilkårligt felt fra 2-12");
         System.out.println("Man vinder ved at opnå 3000 point");
-        System.out.println("");
         System.out.println("");
         System.out.println("");
         System.out.println("");
