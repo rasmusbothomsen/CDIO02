94847af4fccf8990c3188ab72f7011ae05f8ef75
Christoffer Fink <christo.fink.1@gmail.com>
2021-10-26 16:25:52 +0200

    gui update

--- Matador/src/Gui.java
+++ Matador/src/Gui.java
@@ -7,10 +7,13 @@ import java.lang.Object;
 public class Gui {
     public static void main(String[] args) {
 
-        GUI_Street street = new GUI_Street("Tower", "+250", "", "", Color.BLUE, Color.BLACK);
-        GUI_Street[] s = new GUI_Street[]{street};
+        GUI_Street Tower = new GUI_Street("Tower", "+250", "", " ", Color.BLUE, Color.BLACK);
+        GUI_Street Crater = new GUI_Street("Crater", "-100", "", " ", Color.BLUE, Color.BLACK);
+        GUI_Street PlaceGate = new GUI_Street("Place Gate", "+100", "", " ", Color.BLUE, Color.BLACK);
+        GUI_Street ColdDesert = new GUI_Street("Cold Desert", "-20", "", " ", Color.BLUE, Color.BLACK);
+        GUI_Street[] s = new GUI_Street[]{Tower,Crater,PlaceGate,ColdDesert};
         GUI gui = new GUI(s);
+    }
 
     }
 
-}
