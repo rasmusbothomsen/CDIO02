a2a2445bc5e8feb38bcdbbafee56f5997c0b70b4
Christoffer Fink <christo.fink.1@gmail.com>
2021-10-27 13:29:14 +0200

    typo

--- Matador/src/Gui.java
+++ Matador/src/Gui.java
@@ -11,7 +11,7 @@ public class Gui {
         GUI_Street Crater = new GUI_Street("Crater", "-100", "You stumbled into the Crater", " ", Color.PINK, Color.BLACK);
         GUI_Street PlaceGate = new GUI_Street("Place Gate", "+100", "You approach the Palace gates", " ", Color.PINK, Color.BLACK);
         GUI_Street ColdDesert = new GUI_Street("Cold Desert", "-20", "You landed in the Cold Desert", " ", Color.PINK, Color.BLACK);
-        GUI_Street WalledCity = new GUI_Street("Walled City", "+180", "You landed in the Cold Desert", " ", Color.PINK, Color.BLACK);
+        GUI_Street WalledCity = new GUI_Street("Walled City", "+180", "You visited the Walled city", " ", Color.PINK, Color.BLACK);
         GUI_Street[] s = new GUI_Street[]{Tower,Crater,PlaceGate,ColdDesert,WalledCity};
         GUI gui = new GUI(s);
     }
